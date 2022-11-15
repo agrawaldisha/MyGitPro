e2fe5ed73256a2279c234ec9f6646f5653c51487
agrawaldisha <agrawaldisha294@gmail.com>
2022-11-15 11:39:36 +0530

    Initial commit

---
+++ AtriAssignment/style.js
@@ -0,0 +1,19 @@
+alert(search for web);
+// var form=document.getElementById("myForm")
+
+// form.addEventListener('submit',function(e){
+//     e.preventDefault()
+//     var search=document.getElementById("search").value
+//     alert(search)
+    // var originalName=search.split(' ').join('')
+    // document.getElementById("result").innerHTML=""
+    // fetch("https://api.github.com/users/"+originalName)
+    // .then((result) => result.json())
+    // .then((data) => {
+    //     console.log(data)
+    //     document.getElementById("result").innerHTML=`
+    //             <a target="_blank"href="https://www/github.com/${originalName}"><img src="${data.avatar_url}"/></a>
+    //             `
+                
+    // })
+// })
